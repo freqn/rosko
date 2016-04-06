3435bc79183ed780a5ea745806d43fd5ad5c603a
freqn <ceyler@gmail.com>
2016-04-05 21:20:08 -0400

    testing

--- app.rb
+++ app.rb
@@ -12,5 +12,5 @@ def postback(message, channel)
     when "wink" then message = "Dogs wink too."
     end
     slack_webhook = ENV['SLACK_WEBHOOK_URL']
-    HTTParty.post slack_webhook, body: {"text" => @message.to_s, "username" => "Rosko", "channel" => params[:channel_id]}.to_json, headers: {'content-type' => 'application/json'}
+    HTTParty.post slack_webhook, body: {"text" => message.to_s, "username" => "Rosko", "channel" => params[:channel_id]}.to_json, headers: {'content-type' => 'application/json'}
 end
