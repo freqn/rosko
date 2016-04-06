0db8c8eafd9ae60985c2b0b0052c59c9c0dd44ff
freqn <ceyler@gmail.com>
2016-04-05 21:27:18 -0400

    testing

--- app.rb
+++ app.rb
@@ -9,7 +9,12 @@ end
 
 def postback(message, channel)
     case message
-    when "wink" then message = "Dogs wink too."
+    when "wink" then
+      message = "Dogs wink too."
+    when "help" then
+      message = "`/rosko wink` is my only command, but stay tuned because every office puppy learns new tricks."
+    else
+      message = "Umm.. I don't know how to do that. Type `/rosko help` to see what I can do"
     end
     slack_webhook = ENV['SLACK_WEBHOOK_URL']
     HTTParty.post slack_webhook, body: {"text" => message.to_s, "username" => "Rosko", "channel" => params[:channel_id]}.to_json, headers: {'content-type' => 'application/json'}
