0db34b950020e023cfb6400b93187d5c69c76e32
freqn <ceyler@gmail.com>
2016-04-06 21:11:19 -0400

    testing

--- test.rb
+++ test.rb
@@ -1,7 +1,37 @@
-@mystring = "test"
+require 'sinatra'
 
-def get_string
-	puts @mystring
+=begin
+before we process a route, we'll set the response as
+plain text and set up an array of viable moves that
+a player (and the computer) can perform
+=end
+
+before do
+	content_type :txt
+	@defeat = {rock: :scissors, scissors: :paper, paper: :rock}
+	@throws = @defeat.keys
 end
 
-get_string
+get '/throw/:type' do
+	# the params[] hash stores querystring and form data.
+	player_throw = params[:type].to_sym
+
+	# in the case of a player providing a throw that is not valid,
+	# we halt with a status code of 403 (Forbidden) and let them
+	# know they need to make a valid throw to play.
+	if !@throws.include?(player_throw)
+		halt 403, "You must throw one of the following #{@throws}"
+	end
+
+	# now we can select a random throw for the computer
+	computer_throw = @throws.sample
+
+	# compare the player and computer throws to determine a winner
+	if player_throw == computer_throw
+		"You tied with the computer, try again."
+	elsif computer_throw == @defeat[player_throw]
+		"Nicely done; #{player_throw} beats #{computer_throw}!"
+	else
+		"Ouch #{computer_throw} beats #{player_throw}. Better luck next time!"
+	end
+end
